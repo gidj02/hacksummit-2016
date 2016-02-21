76366870d450d44e24824b802542214a6ffd1cc2
Jose <sanjuanjojo@gmail.com>
2016-02-22 04:15:47 +0800

    Refactor Code

--- hacksummit-api/database/seeds/TagTableSeeder.php
+++ hacksummit-api/database/seeds/TagTableSeeder.php
@@ -18,7 +18,7 @@ class TagTableSeeder extends Seeder
 
         $tag = array('Web', 'Mobile', 'Angular', 'React', 'Django', 'Laravel', 'Paypal', 'MasterCard', 'C', 'C#', 'Raspberry', 'Python', 'PHP', 'CodeIgniter', 'CakePHP', 'Flask', 'VanillaJS', 'C++', 'Basic', 'Visual Studio');
 
-        foreach(range(1,20) as $index)
+        foreach(range(0,19) as $index)
         {
             Tag::create(array(
             'tag_name' => $tag[$index],
