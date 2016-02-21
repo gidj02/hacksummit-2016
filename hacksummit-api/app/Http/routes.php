21112e166d7092742fdfadb92736dc079db3f63e
gidj02 <ginodejesus02@gmail.com>
2016-02-21 22:27:34 +0800

    Added CORS provider

--- hacksummit-api/app/Http/routes.php
+++ hacksummit-api/app/Http/routes.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'api/v1', 'middleware' => 'cors'], function(){
+    Route::post('login', 'Auth\OAuthController@login');
+});
